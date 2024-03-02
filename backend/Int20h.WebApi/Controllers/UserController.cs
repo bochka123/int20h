497c96114afe18664457b06fcb6aac62ee07e4b8
Oleksandr Tkach <sasha2003tkach001100@gmail.com>
2024-03-02 18:12:23 +0200

    Finished profile

--- backend/Int20h.WebApi/Controllers/UserController.cs
+++ backend/Int20h.WebApi/Controllers/UserController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Int20h.BLL.Interfaces;
+using Int20h.Common.Dtos.File;
+using Int20h.Common.Dtos.User;
+using Int20h.Common.Response;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,9 +13,65 @@ namespace Int20h.WebApi.Controllers;
 [Authorize]
 public class UserController : ControllerBase
 {
-    [HttpDelete]
-    public IActionResult Delete()
+    private readonly IUserService _userService;
+    private readonly ICredentialService _credentialService;
+
+    public UserController(IUserService userService, ICredentialService credentialService)
     {
-        return Ok();
+        _userService = userService;
+        _credentialService = credentialService;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> EditProfile([FromBody] EditUserDto userDto)
+    {
+        var response = await _userService.UpdateUser(userDto);
+
+        if (response.Status == Status.Success)
+        {
+            return Ok(response);
+        }
+
+        return BadRequest(response);
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteProfile()
+    {
+        var response = await _userService.DeleteUser(_credentialService.UserId);
+
+        if (response.Status == Status.Success)
+        {
+            return NoContent();
+        }
+
+        return BadRequest(response);
+    }
+
+    [HttpPost("addPhoto")]
+    public async Task<ActionResult> AddPhoto()
+    {
+        var formCollection = await Request.ReadFormAsync();
+        var file = formCollection.Files.FirstOrDefault();
+
+        if (file is not null && file.Length > 0)
+        {
+            var result = await _userService.UpdatePhoto(file, _credentialService.UserId);
+            return Ok(result);
+        }
+
+        return BadRequest("No files found in the request.");
+    }
+
+    [HttpDelete("deletePhoto")]
+    public async Task<ActionResult> DeletePhoto([FromBody] FileDto fileDto)
+    {
+        if (fileDto is not null)
+        {
+            var result = await _userService.DeletePhoto(fileDto, _credentialService.UserId);
+            return Ok(result);
+        }
+
+        return BadRequest("No file url.");
     }
 }
