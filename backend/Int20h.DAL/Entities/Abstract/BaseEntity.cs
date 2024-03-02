5f446913a0b6d24b3041c4ea30b119c2858c97d2
lordoleksiy <lordoleksiy@gmail.com>
2024-03-02 14:55:19 +0200

    feature: add user models

--- backend/Int20h.DAL/Entities/Abstract/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Int20h.DAL.Entities.Abstract;
-
-public class BaseEntity
-{
-    public Guid Id { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
-}
