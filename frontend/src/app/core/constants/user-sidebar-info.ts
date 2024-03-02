e352b44000c363717cb87921ff4997b274af31b0
double-hope <nadia.prohorchuk@gmail.com>
2024-03-02 15:39:52 +0200

    add name field

--- frontend/src/app/core/constants/user-sidebar-info.ts
+++ frontend/src/app/core/constants/user-sidebar-info.ts
@@ -4,18 +4,22 @@ export const userSidebarInfo: IUserSidebarInfo[] = [
     {
         availableRoles: [Role.admin],
         route: '/lol',
+        name: 'Test2',
     },
     {
         availableRoles: [Role.teacher, Role.admin],
         route: '/test',
+        name: 'Test3',
     },
     {
         availableRoles: [Role.user],
         route: '/user',
+        name: 'Test1',
     }
 ]
 
 export interface IUserSidebarInfo {
     availableRoles: Role[];
     route: string;
+    name: string;
 }
