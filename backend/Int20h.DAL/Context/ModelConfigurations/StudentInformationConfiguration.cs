095bc184f6e52d148ee9d4885fa33dac8e2026c0
lordoleksiy <lordoleksiy@gmail.com>
2024-03-02 22:55:02 +0200

    add student info config

---
+++ backend/Int20h.DAL/Context/ModelConfigurations/StudentInformationConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Int20h.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Int20h.DAL.Context.ModelConfigurations
+{
+    public class StudentInformationConfiguration : IEntityTypeConfiguration<StudentInformation>
+    {
+        public void Configure(EntityTypeBuilder<StudentInformation> builder)
+        {
+            builder
+                .HasOne(s => s.User)
+                .WithOne()
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder
+                .HasOne(s => s.Group)
+                .WithMany(g => g.Students)
+                .HasForeignKey(s => s.GroupId);
+        }
+    }
+}
