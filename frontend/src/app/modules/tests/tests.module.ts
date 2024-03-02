fd1a5234b5014c684907775a101f8bd88252541a
lordoleksiy <lordoleksiy@gmail.com>
2024-03-02 19:50:20 +0200

    add new component for tests

---
+++ frontend/src/app/modules/tests/tests.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TestsPageComponent } from './tests-page/tests-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    TestsPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TestsModule { }
