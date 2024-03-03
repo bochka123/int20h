4a9280c6b9372913886efd052f25e930ba692293
double-hope <nadia.prohorchuk@gmail.com>
2024-03-03 05:51:56 +0200

    add showing groups

--- frontend/src/app/modules/groups/groups.component.ts
+++ frontend/src/app/modules/groups/groups.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import { AuthService } from '@core/services/auth.service';
 import {GroupsService} from "@core/services/groups.service";
+import { IGroup } from 'src/app/models/IGroup';
 
 @Component({
   selector: 'app-groups',
@@ -7,8 +9,13 @@ import {GroupsService} from "@core/services/groups.service";
   styleUrls: ['./groups.component.scss']
 })
 export class GroupsComponent implements OnInit {
-  constructor(private groupService: GroupsService) {
+
+  availableGroups: IGroup[];
+
+  constructor(private groupService: GroupsService, authService: AuthService) {
+    groupService.getUserGroups({ userEmail: authService.getUserEmail() }).subscribe(res => { if(res.value) this.availableGroups = res.value; });
   }
+
   ngOnInit(): void {
   }
 
