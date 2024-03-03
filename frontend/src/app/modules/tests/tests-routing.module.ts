6409871a814bf39391aa87333d86271c18c816db
lordoleksiy <lordoleksiy@gmail.com>
2024-03-03 08:12:54 +0200

    add create page

--- frontend/src/app/modules/tests/tests-routing.module.ts
+++ frontend/src/app/modules/tests/tests-routing.module.ts
@@ -4,14 +4,21 @@ import { TestsPageComponent } from './tests.component';
 import { AuthGuard } from '@core/guards/auth.guard';
 import { RoleGuard } from '@core/guards/role.guard';
 import { Role } from '@shared/data/roles';
+import {CreatePageComponent} from "@modules/tests/create-page/create-page.component";
 
 const routes: Routes = [
   {
     path: '',
     component: TestsPageComponent,
-    canActivate: [AuthGuard, RoleGuard],
-    data: { roles: [Role.student, Role.teacher, Role.admin] }
+    // canActivate: [AuthGuard, RoleGuard],
+    // data: { roles: [Role.student, Role.teacher, Role.admin] }
   },
+  {
+    path: 'create',
+    component: CreatePageComponent,
+    // canActivate: [AuthGuard, RoleGuard],
+    // data: {roles: [Role.teacher]}
+  }
 ];
 
 @NgModule({
