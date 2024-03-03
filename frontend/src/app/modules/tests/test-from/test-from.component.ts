cc449aad4be26873208b07f4abcc20701ab6f28b
double-hope <nadia.prohorchuk@gmail.com>
2024-03-03 09:24:20 +0200

    questions

--- frontend/src/app/modules/tests/test-from/test-from.component.ts
+++ frontend/src/app/modules/tests/test-from/test-from.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TestsService } from '@core/services/test.service';
+import { TestsService as QuestionServicse } from '@core/services/question.service';
+import { IQuestionDto } from 'src/app/models/IQuestionDto';
 import { ITest } from 'src/app/models/ITest';
 import { ITestDto } from 'src/app/models/ITestDto';
 
@@ -22,12 +24,15 @@ type CustomType = {
 export class TestFromComponent {
   value: string;
   test?: ITestDto;
+  questions?: IQuestionDto[];
 
-  constructor(testService: TestsService, route: ActivatedRoute) {
+  constructor(testService: TestsService, route: ActivatedRoute, questionService: QuestionServicse) {
     testService.getTest({}).subscribe((res) => {
       route.params.subscribe((params) => {
         if (res.value) {
           this.test = res.value.find((t) => t.id == params['id']);
+
+          questionService.get(this.test?.id || '').subscribe(q => this.questions = q);
         }
       });
     });
