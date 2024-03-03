4434d305932b865428c32e630ce6ff38b69ce82f
cyberpOOP <vlad0021135@gmail.com>
2024-03-03 08:21:12 +0200

    create test-form page

--- frontend/src/app/modules/tests/tests-routing.module.ts
+++ frontend/src/app/modules/tests/tests-routing.module.ts
@@ -4,6 +4,7 @@ import { TestsPageComponent } from './tests.component';
 import { AuthGuard } from '@core/guards/auth.guard';
 import { RoleGuard } from '@core/guards/role.guard';
 import { Role } from '@shared/data/roles';
+import {TestFromComponent} from "@modules/tests/test-from/test-from.component";
 
 const routes: Routes = [
   {
@@ -12,6 +13,12 @@ const routes: Routes = [
     canActivate: [AuthGuard, RoleGuard],
     data: { roles: [Role.student, Role.teacher, Role.admin] }
   },
+  {
+    path: 'test',
+    component: TestFromComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: [Role.student, Role.teacher, Role.admin] }
+  }
 ];
 
 @NgModule({
--- frontend/src/app/modules/tests/tests.module.ts
+++ frontend/src/app/modules/tests/tests.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 import { TestsPageComponent } from "./tests.component";
 import { TestsRoutingModule } from "./tests-routing.module";
+import { TestFromComponent } from './test-from/test-from.component';
+import {CommonModule, NgForOf} from "@angular/common";
 
 
 
 @NgModule({
-    declarations: [TestsPageComponent],
-    imports: [TestsRoutingModule],
+    declarations: [TestsPageComponent, TestFromComponent],
+  imports: [TestsRoutingModule, NgForOf, CommonModule],
 })
 
 export class TestsModule {}
